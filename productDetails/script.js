17a79b08ef968ea33fb49b3d0e518a54c866b9df
1501553 <1501553@student.fife.ac.uk>
2016-05-29 15:12:34 +0100

    Removed pseudocode comments in script.js

--- productDetails/script.js
+++ productDetails/script.js
@@ -1,30 +1,3 @@
-/*
-Pseudocode
-Product Page – Order Product
-1.	Get Details
-1.1.	forename = forename input
-// other variables will be similar
-1.2.	name = forename + surname
-1.3.	orderAmount = orderAmount value
-1.4.	product = product name
-
-2.	Create Order
-2.1.	totalPrice = itemPrice * orderAmount
-2.2.	message = name + “your order of“ + orderAmount + product + “will cost” + totalPrice
-
-3.	Display order
-3.1.	print orderMessage
-
-
-Product Page – Stock Check
-1.	Simulate Check
-1.1.	print “Amount loading”
-1.2.	wait for 2 seconds
-
-2.	Display Amount
-2.1.	generate random number
-2.2.	display number
-*/
 function orderProduct() {
 	var orderMessage;
 	
